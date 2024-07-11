caa13b5c6244e537a70d8f039a750e8319035c33
Hamish <hamishapps@gmail.com>
2024-07-11 15:35:41 +1200

    fixed sphere gen (still wip)

--- work/src/sphere.hpp
+++ work/src/sphere.hpp
@@ -3,53 +3,67 @@
 #include <vector>
 
 // project
-#include "cgra_mesh.hpp"
+#include "cgra/cgra_mesh.hpp"
 
-inline mesh_builder create_sphere(int resolution, float radius) {
-  // struct for storing wavefront index data
-  struct wavefront_vertex {
-    unsigned int p, n, t;
-  };
+inline glm::vec3 **generate_sphere_points(int resolution, float radius) {
+  glm::vec3 **arr = new glm::vec3 *[resolution];
 
-  // create reading buffers
-  vector<vec3> positions;
-  vector<vec3> normals;
-  vector<vec2> uvs;
+  for (int i = 0; i < resolution; ++i) {
+    arr[i] = new glm::vec3[resolution];
+    for (int j = 0; j < resolution; ++j) {
+      float lon = (j / resolution) * (glm::pi<float>() / 2);
+      float lat = (i / resolution) * (glm::pi<float>());
+
+      float x = radius * sin(lon) * cos(lat);
+      float y = radius * sin(lon) * sin(lat);
+      float z = radius * cos(lon);
+
+      glm::vec3 v = glm::vec3(x, y, z);
+      arr[i][j] = v;
+    }
+  }
+
+  return arr;
+}
+
+inline cgra::mesh_builder create_sphere(int resolution, float radius) {
 
   // generate sphere points
+  glm::vec3 **points;
+  points = generate_sphere_points(resolution, radius);
 
-  vec3 v = vec3(0, 0, 0)
-
-               positions.push_back(v);
-  normals.push_back(vn);
-  uvs.push_back(vt);
+  std::cout << "points generated" << std::endl;
 
   // if we don't have any normals, create them naively
   // todo create spherical UV's if they don't exist
 
   // create mesh data
-  mesh_builder mb;
+  cgra::mesh_builder mb;
 
-  for (unsigned int i = 0; i < wv_vertices.size(); ++i) {
-    mb.push_index(i);
-    mb.push_vertex(mesh_vertex{positions[wv_vertices[i].p],
-                               normals[wv_vertices[i].n],
-                               uvs[wv_vertices[i].t]});
-  }
+  int count = 0;
 
-  return mb;
-}
-
-inline vector<mesh_vertex> generate_sphere_points(int resolution,
-                                                  float radius) {
-  for (int i = 0; i < resolution; i++) {
+  for (int i = 0; i < resolution - 1; i++) {
     for (int j = 0; j < resolution; j++) {
-      float lon = map(j, 0, resolution, 0, glm::pi / 2);
-      float lat = map(i, 0, resolution, 0, glm::pi);
 
-      float x = radius * sin(lon) * cos(lat);
-      float y = radius * sin(lon) * sin(lat);
-      float z = radius * cos(lon);
+      // index = j ^ (j+1) + (i*2)
+      std::cout << count << std::endl;
+      mb.push_index(count);
+      mb.push_index(count + 1);
+
+      // positions
+      glm::vec3 point1 = points[i][j];
+      glm::vec3 point2 = points[i + 1][j];
+
+      // calulate normals
+
+      mb.push_vertex(cgra::mesh_vertex{point1, glm::vec3(1), glm::vec3(0)});
+      mb.push_vertex(cgra::mesh_vertex{point2, glm::vec3(1), glm::vec3(0)});
+
+      count += 2;
     }
   }
+
+  std::cout << "meshbuilder made" << std::endl;
+
+  return mb;
 }
