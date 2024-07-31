320920c4ede67c323efa093423c3ff07d79b2e1f
Hamish Burke <burkehami@ecs.vuw.ac.nz>
2024-07-31 13:59:03 +1200

    continue cook torrence

--- work/res/shaders/cook_torrence_frag.glsl
+++ work/res/shaders/cook_torrence_frag.glsl
@@ -16,10 +16,25 @@ in VertexData {
 out vec4 fb_color;
 
 void main() {
-	// calculate lighting (hack)
+	vec3 lightPos = vec3(0, 0, 1);
 	vec3 eye = normalize(-f_in.position);
 	float light = abs(dot(normalize(f_in.normal), eye));
-	vec3 color = mix(uColor / 4, uColor, light/2);
+
+	// Cook-Torrence model
+	float roughness = 0.1;
+	float fresnel = 0.1;
+	float k = 0.1;
+	float D = 0.1;
+	float G = 0.1;
+	float F = 0.1;
+
+	// calculate the reflectance
+	vec3 reflectance = (D * G * F) / (4 * dot(normalize(f_in.normal), eye) * dot(normalize(f_in.normal), lightPos));
+
+	vec3 ambient = vec3(0.1, 0.1, 0.1);
+	reflectance += ambient;
+
+	vec3 color = uColor * reflectance;
 
 	// output to the frambuffer
 	fb_color = vec4(color, 1);
